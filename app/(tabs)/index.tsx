8d10b571c7f9b8f00d2effd1f67f9d79dee81088
Sniper0141 <116571645+Sniper0141@users.noreply.github.com>
2025-04-01 11:35:53 +0200

    refactor usestate stuff

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,50 @@
-import { Image, StyleSheet, Platform, View, Button, ActivityIndicator, Alert } from 'react-native';
+import { StyleSheet, View, Button, ActivityIndicator, Alert } from 'react-native';
 import { useState } from 'react';
 
-let buttonActivated: boolean;
-let setButtonActivated: Function;
+
 
 export default function HomeScreen() {
   
-  [buttonActivated, setButtonActivated] = useState(true)
+  const [buttonActivated, setButtonActivated] = useState(true)
+
+  const onClick = () => {
+    if(buttonActivated === false){
+      return;
+    }
   
+    setButtonActivated(false);
+  
+    const currentDateTime = Date.now();
+    try{
+      fetch("https://httpbin.org/post", {
+        method: "POST",
+        body: JSON.stringify({
+          "currentDateTime": currentDateTime,
+          "platform": window.navigator.platform
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8"
+        }
+      }).then(r => r.json()).then(d => d);
+      console.log("Request sent!");
+    }
+    catch(e){
+      let errorMessage = "Failed to send request. ";
+      if (e instanceof Error){
+        errorMessage += "Error message: " + e.message;
+      }
+      console.log(errorMessage);
+  
+      Alert.alert("Sorry, something went wrong", "Failed to send the request.", [
+        {
+          text: "Alright"
+        }
+      ])
+    }
+  
+    setButtonActivated(true);
+  }
+
   return (
     <View style={styles.button}>
       <Button title='Send request' onPress={onClick}/>
@@ -21,41 +58,3 @@ const styles = StyleSheet.create({
     paddingTop: 100,
   }
 });
-
-function onClick(){
-  if(buttonActivated === false){
-    return;
-  }
-
-  setButtonActivated(false);
-
-  const currentDateTime = Date.now();
-  try{
-    const response = fetch("https://httpbin.org/post", {
-      method: "POST",
-      body: JSON.stringify({
-        "currentDateTime": currentDateTime,
-        "platform": window.navigator.platform
-      }),
-      headers: {
-        "Content-type": "application/json; charset=UTF-8"
-      }
-    });
-    console.log("Request sent!");
-  }
-  catch(e){
-    let errorMessage = "Failed to send request. ";
-    if (e instanceof Error){
-      errorMessage += "Error message: " + e.message;
-    }
-    console.log(errorMessage);
-
-    Alert.alert("Sorry, something went wrong", "Failed to send the request.", [
-      {
-        text: "Alright"
-      }
-    ])
-  }
-
-  setButtonActivated(true);
-}
